af378b6adf01a1f6c5aeb5a9e13137f16a09019c
Tuca-PRS <paulorodrigodesouza@live.com>
2024-09-09 22:44:43 -0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,29 @@
+#Configurações iniciais - é necessário instalar o Pygame.
+
+import pygame
+import random
+
+#Iniciando o Pygame.
+
+pygame.init()
+
+#Criando a tela.
+
+pygame.display.set_caption
+
+largura, altura = 1200, 800
+
+tela = pygame.display.set_mode((largura, altura))
+
+relógio = pygame.time.Clock()
+
+#Definindo as cores
+
+preta = (0, 0, 0)
+
+branca = (255, 255, 255)
+
+vermelho = (255, 0, 0)
+
+verde = (0, 255, 0)
+
